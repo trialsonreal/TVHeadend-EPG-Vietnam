6af350900c75637973047e65314f480c4542a9ba
Lê Trung Sơn <88416710+trialsonreal@users.noreply.github.com>
2022-09-09 00:02:44 +0700

    Update epg.sh

--- epg.sh
+++ epg.sh
@@ -4,6 +4,6 @@
 
 wget -O $HOME/xmltv.xmltv http://onetv.click/schedule/epg.xml
 /usr/bin/socat open:$HOME/xmltv.xmltv UNIX-CONNECT:/var/lib/hts/.hts/tvheadend/epggrab/xmltv.sock
-
+/usr/bin/socat open:$HOME/xmltv.xmltv UNIX-CONNECT:/home/hts/.hts/tvheadend/epggrab/xmltv.sock
 #Máy Bot - 2022 
 #Facebook: "https://www.facebook.com/botmynginx"
