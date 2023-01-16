d9727d0a191d30308f3354fb29c7bfe0f147f468
Lê Trung Sơn <88416710+trialsonreal@users.noreply.github.com>
2023-01-17 00:17:29 +0700

    Update epg.sh

--- epg.sh
+++ epg.sh
@@ -2,7 +2,7 @@
 . /etc/profile
 
 
-wget -O $HOME/xmltv.xmltv http://onetv.click/schedule/epg.xml
+wget -O $HOME/xmltv.xmltv http://lichphatsong.xyz/schedule/epg.xml
 /usr/bin/socat open:$HOME/xmltv.xmltv UNIX-CONNECT:/var/lib/hts/.hts/tvheadend/epggrab/xmltv.sock
 /usr/bin/socat open:$HOME/xmltv.xmltv UNIX-CONNECT:/home/hts/.hts/tvheadend/epggrab/xmltv.sock
 #Máy Bot - 2022 
